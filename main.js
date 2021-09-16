d8b0384d278123caee3f198fc6077cdfee181b1e
CoderIsCool <86199879+CoderIsCool@users.noreply.github.com>
2021-09-16 09:22:53 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 canvas = document.getElementById("Paint");
 ctx = canvas.getContext("2d");
 
+nasa_images = ["nasa1.jpeg","nasa2.jpeg","nasa3.jpeg","nasa4.jpeg","nasa5.jpeg","nasa6.jpeg"];
+
+randomnumber = Math.floor(Math.random()* 6);
+console.log(randomnumber);
+
 rover_x = 10;
 rover_y = 10;
 
 rover_width = 100;
 rover_height = 90;
 
-bg = "mars.jpg";
+bg = nasa_images[randomnumber];
+console.log("background image = "+bg);
 roimg = "rover.png";
 
 function rover()
@@ -52,8 +58,45 @@ function my_keydown(e)
     }
     if (keypress =='39')
     {
-        Right();
+         Right();
         console.log("Right");
         
+    }
+}
+function Up()
+{
+    if(rover_y >= 0){
+        rover_y = rover_y - 10;
+        console.log("When up arrow is pressed x = " + rover_x +" and y = " +rover_y);
+        mars();
+        sram();
+
+    }
+}
+function Down()
+{
+    if(rover_y <= 500){
+        rover_y = rover_y + 10;
+        console.log("When down arrow is pressed x = " + rover_x +" and y = " +rover_y);
+        mars();
+        sram();
+    }
+}
+function Left()
+{
+    if(rover_x >= 0){
+        rover_x = rover_x - 10;
+        console.log("When left arrow is pressed x = " + rover_x +" and y = " +rover_y);
+        mars();
+        sram();
+    }
+}
+function Right()
+{
+    if(rover_x <= 700){
+        rover_x = rover_x + 10;
+        console.log("When right arrow is pressed x = " + rover_x +" and y = " +rover_y);
+        mars();
+        sram();
     }
 }
